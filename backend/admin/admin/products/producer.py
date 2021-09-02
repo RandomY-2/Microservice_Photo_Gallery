1a246503fd7963f1238aaddb3841929a35328033
yjh020711@ucla.edu <yjh020711@ucla.edu>
2021-09-02 16:07:50 +0800

    main backend server created using flask

---
+++ backend/admin/admin/products/producer.py
@@ -0,0 +1,11 @@
+import pika, json
+
+params = pika.URLParameters('amqps://keakrtib:XkIR-8xquzQieSr4oVVJx65eQOb4sHC_@beaver.rmq.cloudamqp.com/keakrtib')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish(method, body):
+  properties = pika.BasicProperties(method)
+  channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
--- backend/admin/admin/products/views.py
+++ backend/admin/admin/products/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .producer import publish
+
 from .models import Product, User
 from .serializers import ProductSerializer
 
@@ -17,6 +19,7 @@ class ProductViewSet(viewsets.ViewSet):
     serializer = ProductSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
+    publish('product_created', serializer.data)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
   def retrieve(self, request, primary_key=None):
@@ -29,11 +32,13 @@ class ProductViewSet(viewsets.ViewSet):
     serializer = ProductSerializer(instance=product, data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
+    publish('product_updated', serializer.data)
     return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
   def destroy(self, request, primary_key=None):
     product = Product.objects.get(id=primary_key)
     product.delete()
+    publish('product_deleted', primary_key)
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 class UserAPIView(APIView):
